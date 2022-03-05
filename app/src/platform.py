885f01a6d539843be16fca6d0370fb0e1643eaca
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-03-05 19:20:26 +0200

    Add MacOs support

--- app/src/platform.py
+++ app/src/platform.py
@@ -9,9 +9,15 @@ if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
     MAIN_EXE_NAME = "LabSupportClient.exe"
     DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_Windows.zip"
 
-else:
-    # Linux or other:
+elif sys.platform.startswith('linux'):
+    # Linux:
     UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/download/Latest/Updater_Linux'
     UPDATER_FILE = 'Update'
     MAIN_EXE_NAME = "LabSupportClient"
     DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_Linux.zip"
+elif sys.platform.startswith('darwin'):
+    # MacOS:
+    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/download/Latest/Updater_MacOS'
+    UPDATER_FILE = 'Update'
+    MAIN_EXE_NAME = "LabSupportClient"
+    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_MacOs.zip"
