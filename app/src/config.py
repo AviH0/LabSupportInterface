397a12f87d9d88241401958d246823e57e636861
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-12-15 17:02:01 +0200

    Add android option to settings

--- app/src/config.py
+++ app/src/config.py
@@ -15,6 +15,8 @@ SESSION_LINK = 'Link to Session'
 INVITE_MSG_BODY = 'Invite Message File'
 MAIL_ACCOUNT_CREDS = 'Mail Account Credentials'
 
+IS_ANDROID = 'Adjust for Android'
+
 
 class Settings:
 
@@ -25,7 +27,8 @@ class Settings:
                          SESSION_LINK: '',
                          INVITE_MSG_BODY: os.path.join(os.path.curdir, 'app', 'config',"email_message.txt"),
                          MAIL_ACCOUNT_CREDS: os.path.join(os.path.curdir, 'app', 'credentials',"mail_account_secret.json"),
-                         PATH_TO_CLIENT_SECRET: os.path.join(os.path.curdir, 'app', 'credentials',"client_secret_637398666132-j8s19q7egap0u79l894jmuhauiv39ec7.apps.googleusercontent.com.json")}
+                         PATH_TO_CLIENT_SECRET: os.path.join(os.path.curdir, 'app', 'credentials',"client_secret_637398666132-j8s19q7egap0u79l894jmuhauiv39ec7.apps.googleusercontent.com.json"),
+                         IS_ANDROID: False}
 
         self.new_settings = {key: None for key in self.settings.keys()}
         self.load_configurations()
@@ -41,11 +44,18 @@ class Settings:
         settings_frame = tk.Frame(root)
         index = 0
         for index, key in enumerate(self.settings.keys()):
+            if type(self.settings[key]) == bool:
+                tk.Checkbutton(settings_frame, variable=self.new_settings[key]).grid(row=index, column=1)
             name = tk.Label(settings_frame, text=key, justify=tk.LEFT)
             name.grid(row=index, column=0, padx=5, sticky=tk.W, pady=10)
-            value = tk.Entry(settings_frame)
-            value.insert(0, self.settings[key])
-            value.config(width=100)
+            if type(self.settings[key]) == bool:
+                value = tk.Checkbutton(settings_frame, variable=self.new_settings[key]).grid(row=index, column=1)
+                value.select() if self.settings[key] else value.deselect()
+                value = tk.BooleanVar()
+            else:
+                value = tk.Entry(settings_frame)
+                value.insert(0, self.settings[key])
+                value.config(width=100)
             value.grid(row=index, column=1, sticky=tk.E)
             self.new_settings[key] = value
 
@@ -93,7 +103,12 @@ class Settings:
             for line in lines:
                 index = line.index('=')
                 key, value = line[:index], line[index+1:]
-                self.settings[key] = value.strip()[1:-1]
+                # if key is prefixed with bool: then it is a boolean value
+                if key.startswith('bool:'):
+                    key = key[5:]
+                    self.settings[key] = True if value == 'True' else False
+                else:
+                    self.settings[key] = value.strip()[1:-1]
         else:
             print("Cannot find config file, falling back on default settings.")
 
@@ -109,5 +124,8 @@ class Settings:
                 "// ----------------------------------------------------------------------------------------- //"
                 "\n\n")
             for key in self.settings.keys():
+                # if type is boolean, prefix with bool:
+                if type(self.settings[key]) == bool:
+                    file.write("bool:")
                 file.write(key + '=' + "\"{}\"".format(self.settings[key]) + '\n')
         # print("Configurations saved.")
