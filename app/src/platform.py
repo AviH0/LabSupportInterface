56324115fedfcccf48e7dfdf2ac2657b34b9bf16
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-12-21 14:18:58 +0200

    update python update urls

--- app/src/platform.py
+++ app/src/platform.py
@@ -1,34 +1,46 @@
-
-
 import sys
+
+
+PYTHON_UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/latest/download/Updater_Linux'
+PYTHON_UPDATER_FILE = 'Update'
+PYTHON_MAIN_EXE_NAME = "LabSupportClient"
+PYTHON_DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/latest/download/LS_Linux.zip"
 
 IS_ANDROID = False
 
 if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
     # Windows
-    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/download/Latest/Updater_Windows.exe'
+    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/latest/download/Updater_Windows.exe'
     UPDATER_FILE = 'Update.exe'
     MAIN_EXE_NAME = "LabSupportClient.exe"
-    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_Windows.zip"
+    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/latest/download/LS_Windows.zip"
 
 elif sys.platform.startswith('linux'):
     from os import environ
     if "ANDROID_ARGUMENT" in environ or "ANDROID_ASSETS" in environ or "ANDROID_BOOTLOGO" in environ:
         # Android
         IS_ANDROID = True
-        UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/archive/refs/tags/Latest.zip'
-        UPDATER_FILE = 'python3 Update.py'
-        MAIN_EXE_NAME = "python3 LabSupportClient.py"
-        DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/archive/refs/tags/Latest.zip"
+        UPDATER_URL = PYTHON_UPDATER_URL
+        UPDATER_FILE = PYTHON_UPDATER_FILE
+        MAIN_EXE_NAME = PYTHON_MAIN_EXE_NAME
+        DEFAULT_URL = PYTHON_DEFAULT_URL
+    else:
+        # Linux:
+        UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/latest/download/Updater_Linux'
+        UPDATER_FILE = 'Update'
+        MAIN_EXE_NAME = "LabSupportClient"
+        DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/latest/download/LS_Linux.zip"
 
-    # Linux:
-    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/download/Latest/Updater_Linux'
-    UPDATER_FILE = 'Update'
-    MAIN_EXE_NAME = "LabSupportClient"
-    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_Linux.zip"
 elif sys.platform.startswith('darwin'):
     # MacOS:
-    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/download/Latest/Updater_MacOS'
+    UPDATER_URL = 'https://github.com/AviH0/LabSupportInterface/releases/latest/download/Updater_MacOS'
     UPDATER_FILE = 'Update'
     MAIN_EXE_NAME = "LabSupportClient"
-    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/download/Latest/LS_MacOs.zip"
+    DEFAULT_URL = "https://github.com/AviH0/LabSupportInterface/releases/latest/download/LS_MacOs.zip"
+
+else:
+    # Just use python
+    UPDATER_URL = PYTHON_UPDATER_URL
+    UPDATER_FILE = PYTHON_UPDATER_FILE
+    MAIN_EXE_NAME = PYTHON_MAIN_EXE_NAME
+    DEFAULT_URL = PYTHON_DEFAULT_URL
